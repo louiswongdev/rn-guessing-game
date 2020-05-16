9cb9af75bbc478b0fc28d30d762268de2ace3d7d
Louis <louis@Louiss-Mac-mini.local>
2020-05-16 18:07:42 -0400

    Setup Header and Card component

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Colors from '../constants/colors';
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 80,
+    paddingTop: 30,
+    backgroundColor: Colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 18,
+  },
+});
